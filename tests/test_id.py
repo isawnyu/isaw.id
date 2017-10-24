7e01294f91f9e3e0bafa1d11ed432c1ffca21a93
Tom Elliott <tom.elliott@nyu.edu>
2017-10-24 12:01:05 -0500

    apply GNU Affero GPL

--- tests/test_id.py
+++ tests/test_id.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Written by Tom Elliott (http://www.paregorios.org).
+# Copyright New York University, 2017.
+#
+# This file is part of isaw.id.
+#
+# isaw.it is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# isaw.it is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with isaw.it.  If not, see <http://www.gnu.org/licenses/>.
 """
 Test basic functionality of the isaw.id package
 """
