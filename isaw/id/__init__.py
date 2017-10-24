be03c44d61108fbdd92544048d5c97c5ed0b2d15
Tom Elliott <tom.elliott@nyu.edu>
2017-10-24 03:59:46 -0500

    initial capability

--- isaw/id/__init__.py
+++ isaw/id/__init__.py
@@ -4,22 +4,129 @@
 Manage unique ids for ISAW entities
 """
 
-import better_exceptions
 import logging
 from datetime import datetime
 from hashlib import blake2b
+from os.path import abspath, isdir, join, realpath
+import sys
+import shutil
 
-def make(content, namespace='', date_time=datetime.now(), id_length=3):
 
-    stamp = bytes(date_time.isoformat(), encoding='ascii')
-    if type(content) == str:
-        data = content.encode('utf-8')
-    else:
-        data = bytes(content)
-    data = stamp + data
-    h = blake2b(data, digest_size=id_length)
-    digest = h.hexdigest()
-    if namespace != '':
-        return '/{}/{}'.format(namespace, digest)
-    else:
-        return '/{}'.format(digest)
+DEFAULT_ID_LENGTH = 3
+DEFAULT_NAMESPACE = ''
+MAX_TRIES = 10
+
+class Maker:
+
+    def __init__(
+        self,
+        ensure_unique=True,
+        registry_path=None,
+        namespace=DEFAULT_NAMESPACE,
+        id_length=DEFAULT_ID_LENGTH):
+
+        self.ensure = ensure_unique
+        if self.ensure:
+            if not registry_path:
+                raise ValueError(
+                    'Maker initialized with ensure_unique=True but no '
+                    'registry_path was provided.')
+            else:
+                path = abspath(realpath(registry_path))
+                if isdir(path):
+                    self.registry_path=registry_path
+                    self.registry = {}
+                else:
+                    raise IOError(
+                        'Make initialized with registry_path="{}", but it is '
+                        'not a directory.')
+        self.namespace = namespace
+        self.id_length = id_length
+
+
+    def __del__(self):
+        if self.ensure:
+            for k, v in self.registry.items():
+                path = join(self.registry_path, k)
+                stamp = datetime.now().isoformat()
+                shutil.copy2(path, '{}.{}.bak'.format(path, stamp))
+                with open(path, 'w') as f:
+                    f.write('\n'.join(list(v)))
+
+    def make(
+        self,
+        content,
+        namespace=None,
+        date_time=datetime.now(),
+        id_length=None):
+
+        stamp = bytes(date_time.isoformat(), encoding='ascii')
+        if type(content) == str:
+            data = content.encode('utf-8')
+        else:
+            data = bytes(content)
+        data = stamp + data
+        if id_length is not None:
+            length = id_length
+        else:
+            length = self.id_length
+        h = blake2b(data, digest_size=length)
+        digest = h.hexdigest()
+        if namespace is None:
+            ns = self.namespace
+        else:
+            ns = namespace
+        if self.ensure:
+            tries = 0
+            while not self._ok(ns, digest):
+                if tries == 0:
+                    logger = logging.getLogger(sys._getframe().f_code.co_name)
+                logger.warning(
+                    'hash collision with "{}"'.format(digest))
+                if tries >= MAX_TRIES:
+                    raise ValueError(
+                        'Could not find unique hash after {} tries.'
+                        ''.format(tries))
+                length += 1
+                h = blake2b(data, digest_size=length)
+                digest = h.hexdigest()
+                tries += 1
+            self._register(ns, digest)
+        if ns != '':
+            return '/{}/{}'.format(ns, digest)
+        else:
+            return '/{}'.format(digest)
+
+
+    def _ok(self, ns, digest):
+        r = self._load_register(ns)
+        return not(digest in r)
+
+
+    def _register(self, ns, digest):
+        self.registry[ns].add(digest)
+
+
+    def _load_register(self, ns):
+        try:
+            r = self.registry[ns]
+        except KeyError:
+            logger = logging.getLogger(sys._getframe().f_code.co_name)
+            path = join(self.registry_path, ns)
+            try:
+                f = open(path, 'r')
+            except IOError as e:
+                raise IOError(
+                    'Failed to open register {} from file "{}"'
+                    ''.format(ns, path)
+                    ).with_traceback(e.__traceback__)
+            else:
+                r = set(f.read().splitlines())
+                logger.info(
+                    'Read {} ids from register file "{}"'
+                    ''.format(len(r), path))
+                self.registry[ns] = r
+        return r
+
+
+
--- tests/test_id.py
+++ tests/test_id.py
@@ -4,38 +4,114 @@
 Test basic functionality of the isaw.id package
 """
 
-import better_exceptions
-from airtight.cli import configure_commandline
-from airtight.logging import flog
-import logging
 from datetime import datetime
-from nose.tools import assert_equal
+import glob
 import isaw.id
+from nose.tools import assert_equal, assert_raises
+from os import remove
+from os.path import dirname, join
+import shutil
+
 
 WHEN = datetime(2017, 10, 21, 6, 47, 18, 153304)
 
 def test_make_defaults():
     global WHEN
-    this = isaw.id.make(content='', date_time=WHEN)
+    m = isaw.id.Maker(
+        ensure_unique=False)
+    this = m.make(content='', date_time=WHEN)
     assert_equal(this, '/8c2dcb')
 
 def test_make_content():
     global WHEN
-    this = isaw.id.make(content='foo', date_time=WHEN)
+    m = isaw.id.Maker(
+        ensure_unique=False)
+    this = m.make(content='foo', date_time=WHEN)
     assert_equal(this, '/46ee55')
 
 def test_make_namespace():
     global WHEN
-    this = isaw.id.make(content='', namespace='foo', date_time=WHEN)
+    m = isaw.id.Maker(
+        ensure_unique=False)
+    this = m.make(content='', namespace='foo', date_time=WHEN)
+    assert_equal(this, '/foo/8c2dcb')
+
+def test_make_namespace_default():
+    global WHEN
+    m = isaw.id.Maker(
+        ensure_unique=False,
+        namespace='foo')
+    this = m.make(content='', date_time=WHEN)
     assert_equal(this, '/foo/8c2dcb')
 
 def test_make_namespace_content():
     global WHEN
-    this = isaw.id.make(content='foo', namespace='bar', date_time=WHEN)
+    m = isaw.id.Maker(
+        ensure_unique=False)
+    this = m.make(content='foo', namespace='bar', date_time=WHEN)
+    assert_equal(this, '/bar/46ee55')
+
+def test_make_namespace_default_content():
+    global WHEN
+    m = isaw.id.Maker(
+        ensure_unique=False,
+        namespace='bar')
+    this = m.make(content='foo', date_time=WHEN)
     assert_equal(this, '/bar/46ee55')
 
 def test_make_length():
     global WHEN
-    this = isaw.id.make(content='', date_time=WHEN, id_length=17)
+    m = isaw.id.Maker(
+        ensure_unique=False)
+    this = m.make(content='', date_time=WHEN, id_length=17)
     assert_equal(len(this), 17*2+1)
     assert_equal(this, '/c73b80d0146d6d03679c3077f9f05129db')
+
+def test_make_length_default():
+    global WHEN
+    m = isaw.id.Maker(
+        ensure_unique=False,
+        id_length=17)
+    this = m.make(content='', date_time=WHEN)
+    assert_equal(len(this), 17*2+1)
+    assert_equal(this, '/c73b80d0146d6d03679c3077f9f05129db')
+
+def test_make_namespace_ensure():
+    global WHEN
+    path = join(dirname(__file__), 'data')
+    src = join(path, 'nstest')
+    dest = join(path, 'temptest')
+    shutil.copy(src, dest)
+    m = isaw.id.Maker(registry_path=path)
+    this = m.make(content='foo', namespace='temptest', date_time=WHEN)
+    # expect a collision, so algorithm adds a hex digit to the digest
+    assert_equal(this, '/temptest/ee950528')
+    del(m)
+    remove(dest)
+    for fn in glob.glob(join(path, '*.bak')):
+        remove(fn)
+
+def test_bad_register():
+    global WHEN
+    path = join(dirname(__file__), 'data')
+    m = isaw.id.Maker(registry_path=path)
+    assert_raises(IOError,
+        m.make, content='foo', namespace='bogus', date_time=WHEN)
+
+def test_saved():
+    global WHEN
+    path = join(dirname(__file__), 'data')
+    src = join(path, 'nstest')
+    dest = join(path, 'temptest')
+    shutil.copy(src, dest)
+    contents = 'The cat in the hat'.split()
+    m = isaw.id.Maker(registry_path=path, namespace='temptest')
+    for c in contents:
+        m.make(content=c, date_time=WHEN)
+    del(m)
+    with open(dest, 'r') as f:
+        results = f.read().splitlines()
+    assert_equal(len(contents), len(results)-1)
+    remove(dest)
+    for fn in glob.glob(join(path, '*.bak')):
+        remove(fn)
